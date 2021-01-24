5e9f811a0e8361a334a65bde5301ba93a8f401f4
Naoise_Myler <mylern@tcd.ie>
2021-01-24 16:36:30 +0000

    mongoose added

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express =  require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO ERROR!")
+    })
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
+app.get('/', (req, res) => {
+    res.send('Woof!')
+})
 
 app.listen(3000, () => {
     console.log("App is listening on port 3000!")
