e0789f9c31d6f8774d267ed4c38f47587abe4281
Naoise_Myler <mylern@tcd.ie>
2021-02-01 20:36:45 +0000

    Added connection to MongoDB & made Campground Schema

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const express =  require('express');
 const app = express();
-const path = require('path');
+const path = require('path'); 
 const mongoose = require('mongoose');
+const Campground = require('./models/campground');
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true })
-    .then(() => {
-        console.log("MONGO CONNECTION OPEN!!")
-    })
-    .catch(err => {
-        console.log("OH NO MONGO ERROR!")
-    })
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
 
+ 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('Woof!')
+    res.render('home')
 })
 
+ 
 app.listen(3000, () => {
     console.log("App is listening on port 3000!")
 })
