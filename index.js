836be90355c78e622307549489b2f373a88fe3f6
Naoise_Myler <mylern@tcd.ie>
2021-02-13 21:13:48 +0000

    Campground list with CRUD functionality views: Campground: index, edit, new, show

--- index.js
+++ index.js
@@ -52,11 +52,11 @@ app.get('/campgrounds/:id/edit', async (req, res,) => {
     res.render('campgrounds/edit', { campground });
 });
 
-app.put('/campgrounds/:id', async (req, res,) => {
+app.put('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
-})
+});
 
 app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
